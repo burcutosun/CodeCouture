923843ed8c6caf8d9785fb34c531d533af4bc9d8
burcutosun <124238456+burcutosun@users.noreply.github.com>
2024-05-01 10:53:56 +0300

    Fix ShopCardItem

--- src/components/ShopCardItem.jsx
+++ src/components/ShopCardItem.jsx
@@ -2,8 +2,7 @@ export default function ShopCardItem(props) {
   const { image, index, title } = props;
 
   let containerClass = "relative ";
-  containerClass += index === 3 ? "mb-0" : "mb-6";
-  containerClass += index > 1 ? "lg:row-span-2" : "lg:row-span-1";
+  containerClass += index > 1 ? "lg:row-span-1" : "lg:row-span-2";
 
   return (
     <div className={containerClass} key={index}>
